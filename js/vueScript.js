0b5e05ff861a445386f9d6f3834099737bdb6c89
Alex Hicks <ahicks090@gmail.com>
2018-04-09 21:16:48 -0400

    Initial commit

---
+++ js/vueScript.js
@@ -0,0 +1,23 @@
+import json from '/civData.json'
+
+export default {
+    name: 'app',
+    data () {
+        return {
+            
+        }
+    }
+}
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        message: 'Hello World',
+        civs: [
+            ''
+        ]
+    }
+
+})
