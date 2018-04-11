b612639d4d4b40420f1a35023ff754d37669cf3f
Alex <=ahick090@gmail.com>
2018-04-11 18:03:07 -0400

    Add/Ban logic fully functional.

--- js/script.js
+++ js/script.js
@@ -1,78 +1,179 @@
 $(document).ready(function() {
-	
+	$('#settingsGrid').hide();
 	// get JSON data
 	$.getJSON("civData.json", function (data) {
-        civs = data.civs;
-        numplayers = 3;
+
+        numPlayers = 3;
         numRandoms = 3;
+        numCivs = 34;
+
+        civs = data.civs;
+        vanillaCivs = [];
+        rafCivs = [];
+        dlcCivs = [];
+
+        // Populate civ version arrays
+        for(var i = 0; i < numCivs; i++) {
+            switch (civs[i].expansion) {
+                case 'vanilla':
+                    vanillaCivs.push(civs[i]);
+                    break;
+
+                case 'raf':
+                    rafCivs.push(civs[i]);
+                    break;
+
+                case 'dlc':
+                    dlcCivs.push(civs[i]);
+                    break;
+                
+                case 'default':
+                    break;
+            }
+        }
+        console.log(vanillaCivs);
+        console.log(rafCivs);
+        console.log(dlcCivs);
 
         $('#numPlayersBtn').click(function() {
-            numPlayers = $('#numPlayers').val();
 
-            // Check if numRandoms is undefined or 0. Will default to 3.
-            if (typeof $('#numRandoms').val() !== 'undefined' && $('#numRandoms').val() != 0) {
-                numRandoms = $('#numRandoms').val();
-            }           
+            $('#settingsGrid').show();
             
-            $('#players').html('<form action="/">');
+            if (typeof $('#numPlayers').val() !== 'undefined' && $('#numPlayers').val() > 0 && $('#numPlayers').val() < 13) {
 
-            for (var i = 0; i < numPlayers; i++) {
-                $('#players').append('<div class="col-4 mt-2"><label for="player' + parseInt(i+1) + '">Player ' + parseInt(i+1) + ': </label><input id="p'+i+'" class="ml-2" type="text"></div><div class="col-8"></div>');
+                numPlayers = $('#numPlayers').val();
+
+            } else {
+                alert("Number of players must be 1 - 12. The value will default to 3 otherwise.");
             }
 
-            $('#players').append('<button id="randomize" type="button">Randomize</button></form>');
+            // Check if numRandoms is undefined or 0. Will default to 3.
+            if (typeof $('#numRandoms').val() !== 'undefined' && $('#numRandoms').val() > 0 && $('#numRandoms').val() < 6) {
+                numRandoms = $('#numRandoms').val();
+            } else {
+                alert("Number of randoms must be 1 - 6. The value will default to 3 otherwise (Or 2 if 12 players).");
+            }        
+            
+            $('#players').html('<h5 class="col-12">Enter player names (Optional): </h5>' +
+                                '<form action="/">'
+                            );
+
+            for (var i = 0; i < numPlayers; i++) {
+                $('#players').append('<div class="col-12 col-lg-6 mt-2"><label class="pr-2" for="player' + parseInt(i+1) + '">Player ' + parseInt(i+1) + ': </label><input id="p'+i+'" type="text"></div>');
+            }
+
+            $('#players').append('<button id="randomize" type="button" class="col-12 btn btn-info mt-2">Randomize</button></form>');
+
+            for (var i = 0; i < numCivs; i++) {
+                $('#allCivs').append(
+                    '<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="display: inline-block;">' +
+                        '<input type="checkbox" class="" id="' + civs[i].name + '">' +
+                        '<label class="pl-1" for="' + civs[i].name + '">' + civs[i].name + '</label>' +
+                    '</div>'
+                );
+            }
+
+            
         });
 
         $('body').on('click', '#randomize', function() {
 
-            numCivs = 34;    
             allRandoms = [];
-            rolls = [];
+            rolls = [];      
+            civsUsed = [];
+            civCount = 0;
 
-            for (var i = 0; i < numPlayers; i++){
-                randoms = [];
-                for(var j = 0; j < numRandoms; j++) {
-                    //console.log("Civ " + j);
-                    do {
-
-                        var roll = Math.floor(Math.random() * numCivs);               
-                        //console.log("Roll: " + roll);      
-
-                    } while (rolls.includes(roll));
-
-                    rolls.push(roll);
-                    //console.log("Rolls: " + rolls);
-                    var civName = civs[roll].name;
-                    randoms.push(civName);
+            for (var i = 0; i < numCivs; i++) {
+                if ( $('#' + civs[i].name).prop("checked")) {
+                    civsUsed.push(civs[i]);
+                    civCount++;
                 }
-
-                allRandoms[i] = randoms;
             }
+            
+            if (civCount >= (numPlayers * numRandoms)) {
 
-            console.log("All Randoms: ");
-            console.log(allRandoms);
+                for (var i = 0; i < numPlayers; i++){
+                    randoms = [];
+                    for(var j = 0; j < numRandoms; j++) {
+                        //console.log("Civ " + j);
+                        do {
 
-            //Reset randomsgrid
-            $('#randomsGrid').html('');
-            for(var i = 0; i < numPlayers; i++) {
-                $('#randomsGrid').append(
-                    '<div class="col-3 text-info bg-warning">' +
-                        '<h4>' + $('#p' + i + '').val() + '</h4>' +
-                        '<ul id="p' + i + 'randoms"></ul>'+
-                    '</div>'
-                );
+                            var roll = Math.floor(Math.random() * civCount);               
+                            //console.log("Roll: " + roll);      
 
-                for(var random in allRandoms[i]) {
-                    //console.log(allRandoms[i][random]);
-                    $('#p' + i + 'randoms').append(
-                        '<li>'+
-                            allRandoms[i][random] +
-                        '</li>'
-                    );
+                        } while (rolls.includes(roll));
+
+                        rolls.push(roll);
+                        //console.log("Rolls: " + rolls);
+                        var civName = civsUsed[roll].name;
+                        randoms.push(civName);
+                    }
+
+                    allRandoms[i] = randoms;
                 }
-                
+
+                console.log("All Randoms: ");
+                console.log(allRandoms);
+
+                //Reset randomsgrid
+                $('#randomsGrid').html('');
+                for(var i = 0; i < numPlayers; i++) {
+
+                    $('#randomsGrid').append(
+                        '<div class="col-3 text-info">' +
+                            '<h4 id="p' + i + 'name"></h4>' +
+                            '<ul id="p' + i + 'randoms"></ul>'+
+                        '</div>'
+                    );
+
+                    if($('#p' + i + '').val() != '') {
+                        console.log($('#p' + i + '').val())
+                        $('#p' + i + 'name').html(
+                            $('#p' + i + '').val()
+                        ); 
+                    } else {
+                        console.log("undefined");
+                        $('#p' + i + 'name').html("Player " + parseInt(i+1));
+                    }
+
+                    for(var random in allRandoms[i]) {
+                        //console.log(allRandoms[i][random]);
+                        $('#p' + i + 'randoms').append(
+                            '<li>'+
+                                allRandoms[i][random] +
+                            '</li>'
+                        );
+                    }
+                    
+                }
+            } else {
+
+                alert("Number of civs requested exceeded the number of civs selected. Please change the values and try again");
             }
         });
         
     });
 });
+
+
+versionSelect = function(version) {
+    switch(version) {
+        case 'vanilla':
+            for (var civ in vanillaCivs) {              
+                $('#' + vanillaCivs[civ].name).prop("checked", ! $('#' + vanillaCivs[civ].name).prop("checked"));    
+            }
+            break;
+
+        case 'raf':
+            for (var civ in rafCivs) {
+                $('#' + rafCivs[civ].name).prop("checked", ! $('#' + rafCivs[civ].name).prop("checked")); 
+            }
+            break;
+
+        case 'dlc':
+            for (var civ in dlcCivs) {
+                $('#' + dlcCivs[civ].name).prop("checked", ! $('#' + dlcCivs[civ].name).prop("checked")); 
+            }
+            break;
+    }
+}
